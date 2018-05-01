3d750809766abe47825f236deba1b76a31882492
ZhenghaoFei <holly.fei@foxmail.com>
2018-05-01 01:45:54 -0700

    read ground pose and plot with estimation

--- main.cpp
+++ main.cpp
@@ -20,6 +20,41 @@
 
 using namespace cv;
 
+double getAbsoluteScale(int frame_id)    {
+  
+  std::string line;
+  int i = 0;
+  std::ifstream myfile ("/Users/holly/Downloads/KITTI/poses/00.txt");
+  double x =0, y=0, z = 0;
+  double x_prev, y_prev, z_prev;
+  if (myfile.is_open())
+  {
+    while (( std::getline (myfile,line) ) && (i<=frame_id))
+    {
+      z_prev = z;
+      x_prev = x;
+      y_prev = y;
+      std::istringstream in(line);
+      //cout << line << '\n';
+      for (int j=0; j<12; j++)  {
+        in >> z ;
+        if (j==7) y=z;
+        if (j==3)  x=z;
+      }
+      
+      i++;
+    }
+    myfile.close();
+  }
+
+  else {
+    std::cout << "Unable to open file";
+    return 0;
+  }
+
+  return sqrt((x-x_prev)*(x-x_prev) + (y-y_prev)*(y-y_prev) + (z-z_prev)*(z-z_prev)) ;
+
+}
 
 void featureDetection(Mat image, std::vector<Point2f>& points, std::vector<KeyPoint>& keypoints)  {   //uses FAST as of now, modify parameters as necessary
   int fast_threshold = 20;
@@ -64,34 +99,34 @@ void drawFeaturePoints(Mat image, std::vector<Point2f>& points){
 }
 
 
-Mat loadImageLeft(int timestep){
+Mat loadImageLeft(int frame_id){
     char filename[200];
-    sprintf(filename, "/Users/holly/Downloads/KITTI/sequences/00/image_0/%06d.png", timestep);
+    sprintf(filename, "/Users/holly/Downloads/KITTI/sequences/00/image_0/%06d.png", frame_id);
     Mat image = imread(filename);
     cvtColor(image, image, COLOR_BGR2GRAY);
 
     return image;
 }
 
-Mat loadImageRight(int timestep){
+Mat loadImageRight(int frame_id){
     char filename[200];
-    sprintf(filename, "/Users/holly/Downloads/KITTI/sequences/00/image_1/%06d.png", timestep);
+    sprintf(filename, "/Users/holly/Downloads/KITTI/sequences/00/image_1/%06d.png", frame_id);
     Mat image = imread(filename);
     cvtColor(image, image, COLOR_BGR2GRAY);
 
     return image;
 }
 
-void visualOdometry(int current_timestep, Mat& rotation, Mat& translation)
+void visualOdometry(int current_frame_id, Mat& rotation, Mat& translation)
 {
-    std::cout << "Timestep " << current_timestep << std::endl;
+
 
     // load images
-    Mat image_left_t0 = loadImageLeft(current_timestep);
-    Mat image_right_t0 = loadImageRight(current_timestep);
+    Mat image_left_t0 = loadImageLeft(current_frame_id);
+    Mat image_right_t0 = loadImageRight(current_frame_id);
 
-    Mat image_left_t1 = loadImageLeft(current_timestep+1);
-    Mat image_right_t1 = loadImageRight(current_timestep+1);
+    Mat image_left_t1 = loadImageLeft(current_frame_id+1);
+    Mat image_right_t1 = loadImageRight(current_frame_id+1);
 
 
     // feature detection using FAST
@@ -169,17 +204,25 @@ void visualOdometry(int current_timestep, Mat& rotation, Mat& translation)
 
 }
 
-void integrateOdometry(Mat& pose, Mat& Rpose, const Mat& rotation, const Mat& translation)
+void integrateOdometry(int frame_id, Mat& pose, Mat& Rpose, const Mat& rotation, const Mat& translation)
 {
-    pose = pose + Rpose * translation;
+    double scale = 1.00;
+    scale = getAbsoluteScale(frame_id);
+
+    pose = pose + scale * Rpose * translation;
     Rpose = rotation * Rpose;
 }
 
-void display(Mat& trajectory, Mat& pose)
+void display(Mat& trajectory, Mat& pose, Mat& pose_gt)
 {
     int x = int(pose.at<double>(0)) + 300;
     int y = int(pose.at<double>(2)) + 100;
     circle(trajectory, Point(x, y) ,1, CV_RGB(255,0,0), 2);
+
+    x = int(pose_gt.at<double>(0)) + 300;
+    y = int(pose_gt.at<double>(2)) + 100;
+    circle(trajectory, Point(x, y) ,1, CV_RGB(255,255,0), 2);
+
     imshow( "Trajectory", trajectory );
 
 
@@ -191,35 +234,41 @@ int main(int argc, char const *argv[])
 
     char filename_pose[200];
     sprintf(filename_pose, "/Users/holly/Downloads/KITTI/poses/00.txt");
-    std::vector<Matrix> pose_ground_truth = loadPoses(filename_pose);
+    std::vector<Matrix> pose_matrix_gt = loadPoses(filename_pose);
 
-    std::cout << "left t0 feature point size after tracking " << points_left_t0.size() << std::endl;
+    // Mat rotation, translation;
+    Mat rotation = Mat::eye(3, 3, CV_64F);
+    Mat translation = Mat::zeros(3, 1, CV_64F);
+    Mat pose = Mat::zeros(3, 1, CV_64F);
+    Mat Rpose = Mat::eye(3, 3, CV_64F);
+    Mat pose_gt = Mat::zeros(1, 3, CV_64F);
 
-    // // Mat rotation, translation;
-    // Mat rotation = Mat::eye(3, 3, CV_64F);
-    // Mat translation = Mat::zeros(3, 1, CV_64F);
-    // Mat pose = Mat::zeros(3, 1, CV_64F);
-    // Mat Rpose = Mat::eye(3, 3, CV_64F);
-    // Mat trajectory = Mat::zeros(600, 600, CV_8UC3);
+    Mat trajectory = Mat::zeros(600, 600, CV_8UC3);
 
-    // for (int timestep = 0; timestep < 1000; timestep++)
-    // {
-    //     visualOdometry(timestep, rotation, translation);
+    for (int frame_id = 0; frame_id < 1000; frame_id++)
+    {
 
-    //     integrateOdometry(pose, Rpose, rotation, translation);
+        std::cout << "frame_id " << frame_id << std::endl;
 
-    //     // std::cout << "R" << std::endl;
-    //     // std::cout << rotation << std::endl;
-    //     // std::cout << "t" << std::endl;
-    //     // std::cout << translation << std::endl;
-    //     std::cout << "Pose" << std::endl;
-    //     std::cout << pose << std::endl;
+        visualOdometry(frame_id, rotation, translation);
+        integrateOdometry(frame_id, pose, Rpose, rotation, translation);
 
-    //     display(trajectory, pose);
+        // std::cout << "R" << std::endl;
+        // std::cout << rotation << std::endl;
+        // std::cout << "t" << std::endl;
+        // std::cout << translation << std::endl;
+        std::cout << "Pose" << std::endl;
+        std::cout << pose << std::endl;
+
+        pose_gt.at<double>(0) = pose_matrix_gt[frame_id].val[0][3];
+        pose_gt.at<double>(1) = pose_matrix_gt[frame_id].val[0][7];
+        pose_gt.at<double>(2) = pose_matrix_gt[frame_id].val[0][11];
+ 
+        display(trajectory, pose, pose_gt);
 
 
-    // }
+    }
 
-    // return 0;
+    return 0;
 }
 
