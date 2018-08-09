f5942f9da5da6a63154753c70f7123c955eb152b
ZhenghaoFei <holly.fei@foxmail.com>
2018-08-08 17:54:34 -0700

    minor change

--- src/utils.cpp
+++ src/utils.cpp
@@ -88,7 +88,7 @@ void display(int frame_id, cv::Mat& trajectory, cv::Mat& pose, std::vector<Matri
 {
     // draw estimated trajectory 
     int x = int(pose.at<double>(0)) + 300;
-    int y = int(pose.at<double>(2)) + 100;
+    int y = int(pose.at<double>(1)) + 100;
     circle(trajectory, cv::Point(x, y) ,1, CV_RGB(255,0,0), 2);
 
     if (show_gt)
@@ -100,7 +100,7 @@ void display(int frame_id, cv::Mat& trajectory, cv::Mat& pose, std::vector<Matri
       pose_gt.at<double>(1) = pose_matrix_gt[frame_id].val[0][7];
       pose_gt.at<double>(2) = pose_matrix_gt[frame_id].val[0][11];
       x = int(pose_gt.at<double>(0)) + 300;
-      y = int(pose_gt.at<double>(2)) + 100;
+      y = int(pose_gt.at<double>(1)) + 100;
       circle(trajectory, cv::Point(x, y) ,1, CV_RGB(255,255,0), 2);
     }
     // print info
@@ -169,7 +169,7 @@ void integrateOdometryStereo(int frame_i, cv::Mat& frame_pose, const cv::Mat& ro
                         + (translation_stereo.at<double>(2))*(translation_stereo.at<double>(2))) ;
 
     // frame_pose = frame_pose * rigid_body_transformation;
-    std::cout << "scale" << scale << std::endl;
+    std::cout << "scale: " << scale << std::endl;
 
     // if ((scale>0.1)&&(translation_stereo.at<double>(2) > translation_stereo.at<double>(0)) && (translation_stereo.at<double>(2) > translation_stereo.at<double>(1))) 
     if (scale>0.1) 
